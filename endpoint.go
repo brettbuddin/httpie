961bbbbb733cc49a681fa19bc718f4252eed12f0
Brett Buddin <brett@brettbuddin.us>
2013-06-21 22:40:11 -0400

    Endpoint docs

--- endpoint.go
+++ endpoint.go
@@ -7,25 +7,31 @@ import (
     "bytes"
 )
 
+// Endpoint is implemented to provide delayed
+// attachment of the URL/Method/Body of a request
 type Endpoint interface {
     ApplyTo(*http.Request)
 }
 
+// Represents an HTTP GET
 type Get struct {
     *url.URL
 }
 
+// ApplyTo sets the requests Method to GET and URL
 func (g Get) ApplyTo(req *http.Request) {
     req.Method = "GET"
     req.URL    = g.URL
 }
 
+// Represents an HTTP POST
 type Post struct {
     *url.URL
     Body []byte
     ContentType string
 }
 
+// ApplyTo sets the requests Method to POST, URL and Body
 func (p Post) ApplyTo(req *http.Request) {
     req.Method        = "POST"
     req.URL           = p.URL
@@ -34,12 +40,14 @@ func (p Post) ApplyTo(req *http.Request) {
     req.Header.Set("Content-Type", p.ContentType)
 }
 
+// Represents an HTTP PUT
 type Put struct {
     *url.URL
     Body []byte
     ContentType string
 }
 
+// ApplyTo sets the requests Method to PUT, URL and Body
 func (p Put) ApplyTo(req *http.Request) {
     req.Method        = "PUT"
     req.URL           = p.URL
@@ -48,10 +56,12 @@ func (p Put) ApplyTo(req *http.Request) {
     req.Header.Set("Content-Type", p.ContentType)
 }
 
+// Represents an HTTP DELETE
 type Delete struct {
     *url.URL
 }
 
+// ApplyTo sets the requests Method to DELETE and URL
 func (d Delete) ApplyTo(req *http.Request) {
     req.Method = "DELETE"
     req.URL    = d.URL
